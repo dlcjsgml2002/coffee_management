86d02f26856c3f6c6b88267e6a77766f919f3a98
fpvks <rephanoise@naver.com>
2018-11-16 22:25:01 +0900

    커밋 테스트

---
+++ src/main/java/coffee_management/ui/CommitTest.java
@@ -0,0 +1,14 @@
+package coffee_management.ui;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CommitTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
